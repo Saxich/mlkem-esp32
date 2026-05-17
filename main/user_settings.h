b9fb0797426eec644a89dc50dc051f12929c5446
Saxich <mchl.saxa@gmail.com>
2026-05-17 16:32:45 +0200

    explaining comments

--- main/user_settings.h
+++ main/user_settings.h
@@ -2,21 +2,38 @@
 #ifndef BAKALARKA_USER_SETTINGS_H
 #define BAKALARKA_USER_SETTINGS_H
 
-#define MLKEM_K 4
+/*
+ * ML-KEM security level selection:
+ *   2 = ML-KEM-512
+ *   3 = ML-KEM-768
+ *   4 = ML-KEM-1024
+ */
+#define MLKEM_K 3
 
-#define SPEED
-// #define SPEED_DUALCORE
+
+/*
+ * Optimization variant selection
+ * Choose exactly ONE option.
+ *
+ * If none is selected, SPEED is enabled automatically.
+ */
+
+// #define SPEED
+#define SPEED_DUALCORE
 
 // #define STACK_XTREME
 // #define STACK
 // #define STACK_DUALCORE
 
 
-// 1 = Benchmark task = speed+mem
-// 3 = KATtest
-// 4 = KATtest + benchmark
-// 10 = Generate vectors
-#define TEST_TO_TURN  3
+/*
+ * Test selection:
+ *   1  = Benchmark task (speed + memory)
+ *   3  = KAT test
+ *   4  = KAT test + benchmark
+ *   10 = Generate vectors
+ */
+#define TEST_TO_TURN  1
 
 
 #endif
